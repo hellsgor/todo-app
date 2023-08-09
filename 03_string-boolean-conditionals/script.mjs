86e390adf424ba34a42934587f0607c01e79d15c
hellsgor <hellsgor@gmail.com>
2023-08-09 10:09:54 +0400

    task 3.3 is ready

--- 03_string-boolean-conditionals/script.mjs
+++ 03_string-boolean-conditionals/script.mjs
@@ -1,5 +1,6 @@
 import {passwordCheck} from "./scripts/task1.mjs";
 import {fullNameConversion} from "./scripts/task2.mjs";
+import {evenCheck} from "./scripts/task3.mjs";
 
 console.group('%cЗадача 3.1:', 'background-color: red; color: white;');
 const task3_1Data = [
@@ -17,4 +18,9 @@ console.groupEnd();
 
 console.group('%cЗадача 3.2:', 'background-color: red; color: white;');
 const task3_2Result = fullNameConversion(false);
+console.groupEnd();
+
+console.group('%cЗадача 3.3:', 'background-color: red; color: white;');
+const task3_3Data = [2, 5, 8];
+const task3_3Result = evenCheck(task3_3Data);
 console.groupEnd();
