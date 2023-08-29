372f7ae061f35c767251566502e96e0ef176d036
hellsgor <hellsgor@gmail.com>
2023-08-29 22:56:21 +0400

    task 4 is ready

--- 04_arrays-and-loops/script.mjs
+++ 04_arrays-and-loops/script.mjs
@@ -1,6 +1,7 @@
 import {getTask4_2Result} from "./scripts/task2.mjs";
 import {getTask4_1Result} from "./scripts/task1.mjs";
 import {getTask4_3Result} from "./scripts/task3.mjs";
+import {getTask4_4Result} from "./scripts/task4.mjs";
 
 console.group('%cЗадача 4.1:', 'background-color: red; color: white;');
 const task4_1Data = [
@@ -20,4 +21,12 @@ console.groupEnd();
 console.group('%cЗадача 4.3:', 'background-color: red; color: white;');
 const task4_3Data = [3, 1, 7];
 const task4_3Result = getTask4_3Result(task4_3Data, task4_2Result);
+console.groupEnd();
+
+console.group('%cЗадача 4.4:', 'background-color: red; color: white;');
+const task4_4Data = {
+  array1: [2, 2, 17, 21, 45, 12, 54, 31, 53],
+  array2: [12, 44, 23, 5],
+}
+const task4_4Result = getTask4_4Result(task4_4Data);
 console.groupEnd();
