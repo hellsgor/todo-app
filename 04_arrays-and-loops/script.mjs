984d69f2cd7a39a47422dcfc7d063c7fbd9de7ac
hellsgor <hellsgor@gmail.com>
2023-08-29 21:58:33 +0400

    task 2 is ready

--- 04_arrays-and-loops/script.mjs
+++ 04_arrays-and-loops/script.mjs
@@ -1,3 +1,4 @@
+import {getTask4_2Result} from "./scripts/task2.mjs";
 import {generate} from "./scripts/task1.mjs";
 
 console.group('%cЗадача 4.1:', 'background-color: red; color: white;');
@@ -8,6 +9,11 @@ const task4_1Data = [
   {n: -3, m: -10, count: 42},
 ];
 const task4_1Result = generate(task4_1Data);
+console.groupEnd();
+
+console.group('%cЗадача 4.2:', 'background-color: red; color: white;');
+const task4_2Data = [5, 7, 3];
+const task4_2Result = getTask4_2Result(task4_2Data);
 console.groupEnd();
 
 
