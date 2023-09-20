ecafad7f2bf3ee376bc5edc3b02194ff94bdff4e
hellsgor <hellsgor@gmail.com>
2023-09-20 21:49:07 +0400

    third task is ready

--- 05_functions/script.mjs
+++ 05_functions/script.mjs
@@ -2,6 +2,7 @@
 
 import {getTask5_1Result} from "./scripts/task1.mjs";
 import {getTask5_2Result} from "./scripts/task2.mjs";
+import {getTask5_3Result} from "./scripts/task3.mjs";
 
 console.group('%cЗадача 5.1:', 'background-color: red; color: white;');
 const task5_1Data = [1998, 1991, 2007];
@@ -14,4 +15,15 @@ const task5_2Data = {
   blackList: ['jsfunc@mail.ru', 'goodday@day.ru'],
 };
 const task5_2Result = getTask5_2Result(task5_2Data);
+console.groupEnd();
+
+console.group('%cЗадача 5.3:', 'background-color: red; color: white;');
+const task5_3Data = [
+  [2, 5, 1, 3, 4],
+  [12, 33, 3, 44, 100],
+  [0, 1],
+  [3, 2, 1, 3, 0],
+  [-22, 13, 2, -3, 42],
+];
+const task5_3Result = getTask5_3Result(task5_3Data);
 console.groupEnd();
